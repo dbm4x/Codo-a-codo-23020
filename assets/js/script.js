e0a5b659b3c561a416b8d6b4ca1a6db38c2b1669
dbm4x <67777721+dbm4x@users.noreply.github.com>
2023-04-21 15:08:46 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,10 @@ const fechaFormato = fechaActual.toLocaleDateString('es-ES', { day: 'numeric', m
 const fechaElemento = document.getElementById('fecha-actual');
 fechaElemento.setAttribute('datetime', fechaActual.toISOString());
 fechaElemento.textContent = fechaFormato;
+
+if ((".loader").length) {
+    // show Preloader until the website ist loaded
+    $(window).on('load', function () {
+      $(".loader").fadeOut("slow");
+    });
+}
