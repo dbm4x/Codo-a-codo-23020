e4190b4b0fea4d5fe09b8b96d5bf04a15cd5c5bf
dbm4x <67777721+dbm4x@users.noreply.github.com>
2023-04-22 12:01:52 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,50 @@ if ((".loader").length) {
       $(".loader").fadeOut("slow");
     });
 }
+
+/* PARA CONSUMIR API DE LA BUENARDA
+
+// Creamos una instancia del objeto XMLHttpRequest
+var xhr = new XMLHttpRequest();
+
+// Definimos la URL de la API
+var url = "https://api.example.com/users";
+
+// Configuramos la solicitud XHR
+xhr.open("GET", url, true);
+xhr.setRequestHeader("Content-Type", "application/json");
+
+// Enviamos la solicitud
+xhr.send();
+
+// Esperamos a la respuesta de la API
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    // Procesamos la respuesta de la API
+    var response = JSON.parse(xhr.responseText);
+    
+    // Creamos una tabla HTML para mostrar los datos
+    var table = document.createElement("table");
+    
+    // Creamos una fila para el encabezado de la tabla
+    var headerRow = table.insertRow(0);
+    var header1 = headerRow.insertCell(0);
+    var header2 = headerRow.insertCell(1);
+    header1.innerHTML = "Nombre";
+    header2.innerHTML = "Correo electrónico";
+    
+    // Agregamos filas con los datos de los usuarios
+    for (var i = 0; i < response.length; i++) {
+      var row = table.insertRow(i + 1);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      cell1.innerHTML = response[i].name;
+      cell2.innerHTML = response[i].email;
+    }
+    
+    // Agregamos la tabla al elemento HTML
+    document.getElementById("myTable").appendChild(table);
+  }
+};
+
+*/
